711c647a765c070b63cb84926a52721e197c4623
AYODEJI ABOLAJI ALFRED <abolajiayodeji93@gmail.com>
2024-07-20 14:11:45 +0100

    I added cart page and make add and remove from cart functional

--- savorly-frontend/src/components/navbar/Navbar.jsx
+++ savorly-frontend/src/components/navbar/Navbar.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState('home');
 
+  const { getTotalCartAmount } = useContext(StoreContext);
+
   return (
     <div className='navbar'>
-      <h2 className='logo'>Savorly.</h2>
+      <Link to='/'>
+        <h2 className='logo'>Savorly.</h2>
+      </Link>
       <ul className='navbar-menu'>
         <Link
           to='/'
@@ -43,9 +48,11 @@ const Navbar = ({ setShowLogin }) => {
       <div className='navbar-right'>
         <img src={assets.search_icon} alt='' />
         <div className='navbar-search-icon'>
-          <img src={assets.basket_icon} alt='' />
+          <Link to='/cart'>
+            <img src={assets.basket_icon} alt='' />
+          </Link>
 
-          <div className='dot'></div>
+          <div className={getTotalCartAmount() === 0 ? '' : 'dot'}></div>
         </div>
 
         <button onClick={() => setShowLogin(true)} className='btn'>
