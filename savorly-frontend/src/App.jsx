3787f6a986223ae1315a2e8c04fc3b1b07a41a72
AYODEJI ABOLAJI ALFRED <abolajiayodeji93@gmail.com>
2024-07-20 12:39:57 +0100

    I added login pop up and signup using condition

--- savorly-frontend/src/App.jsx
+++ savorly-frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Cart from './pages/cart/Cart';
 import PlaceOrder from './pages/place-order/PlaceOrder';
 import Footer from './components/footer/Footer';
+import LoginPopUp from './components/login/LoginPopUp';
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+      {showLogin ? <LoginPopUp setShowLogin={setShowLogin} /> : <></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
--- savorly-frontend/src/components/navbar/Navbar.jsx
+++ savorly-frontend/src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState('home');
 
   return (
@@ -48,7 +48,9 @@ const Navbar = () => {
           <div className='dot'></div>
         </div>
 
-        <button className='btn'>Sign In</button>
+        <button onClick={() => setShowLogin(true)} className='btn'>
+          Sign In
+        </button>
       </div>
     </div>
   );
