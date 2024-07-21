8b89b1e5da092c6dd5c292f4879e7b3c1123946a
AYODEJI ABOLAJI ALFRED <abolajiayodeji93@gmail.com>
2024-07-21 16:07:51 +0100

    I created admin folder and install dependenciees, router, and pages for the admin of this project

---
+++ admin/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className='app-content'>
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ admin/src/pages/add/Add.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './add.css';
+
+const Add = () => {
+  return <div>Add</div>;
+};
+
+export default Add;
