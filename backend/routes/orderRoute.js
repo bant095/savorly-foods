28cc3ba5740fc0d273ba34cc45aa102643c0f3de
AYODEJI ABOLAJI ALFRED <abolajiayodeji93@gmail.com>
2024-07-24 19:45:45 +0100

    I created an order endpoint for my backend at ordercontroller file and consume it in my admin panel and alo create ui for the order list

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,6 +4,7 @@ import {
   placeOrder,
   verifyOrder,
   userOrders,
+  listOrders,
 } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
@@ -12,5 +13,6 @@ const orderRouter = express.Router();
 orderRouter.post('/place', authMiddleware, placeOrder);
 orderRouter.post('/verify', verifyOrder);
 orderRouter.post('/userorders', authMiddleware, userOrders);
+orderRouter.get('/list', listOrders);
 
 export default orderRouter;
